920827605596a5dc9cfa1cae4568b020f2dd2255
Laura Frank <ljfrank@gmail.com>
2017-03-08 13:05:14 +0100

    Users don't have access to the key to unencrypt these arguments

--- 20.build-args/test-build-arg.sh
+++ 20.build-args/test-build-arg.sh
@@ -7,17 +7,3 @@ else
   echo "UNENCRYPTED has been set to $UNENCRYPTED"
 fi
 
-if [[ $ENCRYPTED == "default" ]] ; then
-  echo "Encrypted build args are not working. ENCRYPTED should not be default."
-  exit 1
-else
-  echo "ENCRYPTED has been set to $ENCRYPTED"
-fi
-
-if [[ $FROMFILE == "default" ]] ; then
-  echo "Encrypted build args file is not working. FROMFILE should not be default."
-  exit 1
-else
-  echo "FROMFILE has been set to $FROMFILE"
-fi
-
