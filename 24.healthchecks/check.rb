10a8146567495efbff4d461f18eeefb9d9695d72
Mark Phelps <mark.aaron.phelps@gmail.com>
2018-01-17 12:25:46 -0500

    Updating main services/steps for healthchecks example

--- 24.healthchecks/check.rb
+++ 24.healthchecks/check.rb
@@ -2,6 +2,6 @@ require 'pg'
 
 postgres_username = 'postgres'
 postgres_password = ''
-test = PG.connect('postgres', 5432, '', '', 'postgres', postgres_username, postgres_password)
+test = PG.connect('postgreshealthcheck', 5432, '', '', 'postgres', postgres_username, postgres_password)
 puts test.exec('SELECT version();').first['version']
 puts 'Healthy!'
